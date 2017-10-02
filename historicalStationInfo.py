44a706062f26c82a77ab04efafcb9ac6de7e7a17
ianvonseggern1 <ianvonseggern@gmail.com>
2017-10-02 15:42:00 -0400

    Add the ability to get station availability for a range of itmes

--- historicalStationInfo.py
+++ historicalStationInfo.py
@@ -1,11 +1,20 @@
 import parseLiveCitibikeData as parse
 import citibikeStationLocation as location
 
-def getStationDataForTime(time):
-  data = parse.readFromFileToDictionary(parse.filePathForTime(time))
+def getStationDataForTime(time, directory):
+  data = parse.readFromFileToDictionary(directory + parse.filePathForTime(time))
   locations = location.getStationLocations()
   for station_id in data:
     if station_id in locations:
       data[station_id]['lat'] = locations[station_id]['lat']
       data[station_id]['lon'] = locations[station_id]['lon']
   return data
+
+# from_time and to_time are minutes past midnight
+# returns a dictionary mapping time (minutes past midnight) to a dictionary of
+# stations to bikes
+def getStationData(date, directory, from_time, to_time, increments):
+  rtn = {}
+  from time in range(from_time, to_time, increments):
+    rtn[time] = getStationDataForTime(time, directory)
+  return rtn
